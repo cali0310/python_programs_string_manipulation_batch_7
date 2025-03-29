d27c17e96b17c5bb88e762a26597b146c84e0715
unknown <ramosyazmien2@gmail.com>
2025-03-29 12:50:58 +0800

    remove suffix

--- remove_suffix.py
+++ remove_suffix.py
@@ -3,3 +3,13 @@
 # remove suffix 
 # return remaining string characters
 # print string
+
+def remove_suffix(string, suffix):
+    if string.endswith(suffix):
+        return string[:-len(suffix)]
+    return string
+
+user_input_string = input("Enter string: ")
+suffix_to_remove = input("Enter suffix to remove: ")
+no_suffix = remove_suffix(user_input_string, suffix_to_remove)
+print(f'Result: "{no_suffix}"')
