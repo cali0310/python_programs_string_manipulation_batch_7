9a130f043bdaf4a26d77eef9bb18b7204293ab44
unknown <ramosyazmien2@gmail.com>
2025-03-29 12:47:37 +0800

    remove remaining spaces

--- remove_space_without_rstrip.py
+++ remove_space_without_rstrip.py
@@ -2,3 +2,13 @@
 # input string
 # remove spaces at the end of the string
 # print string
+
+def remove_trailing_space(string):
+    for i in range(len(string) - 1, -1, -1):
+        if string[i] != " ":
+            return string[:i + 1]
+    return ""
+
+user_input_string = input("Enter string: ")
+no_trailing_space = remove_trailing_space(user_input_string)
+print(f'Result: "{no_trailing_space}"')
