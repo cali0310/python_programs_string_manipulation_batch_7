f76722ca65a46ea6a1bcd3ac032b185e898bbdc5
unknown <ramosyazmien2@gmail.com>
2025-04-01 11:34:28 +0800

    pseudocode

---
+++ index_function.py
@@ -0,0 +1,4 @@
+# create a program that does the same functionality without using index() function.
+# ask user for input
+# loop through the string to find the first occurrence of the substring
+# print the index or an error message if not found
