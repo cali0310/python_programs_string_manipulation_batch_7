38ecd3ed0c343e2cf896c771a8668f05d2735b23
unknown <ramosyazmien2@gmail.com>
2025-04-01 11:16:06 +0800

    convert to upper

--- upper_program.py
+++ upper_program.py
@@ -2,3 +2,7 @@
 # ask user input
 # convert to upper without upper() function
 # print string
+
+user_input_string = str(input("Enter string: ")).lower()
+upper_string = user_input_string.swapcase()
+print(upper_string)
