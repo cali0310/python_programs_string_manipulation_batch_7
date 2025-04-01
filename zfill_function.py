fa4f1333644d703765520058d0a91a80a9f19b0c
unknown <ramosyazmien2@gmail.com>
2025-04-01 11:27:54 +0800

    add 0 characters in beginning

--- zfill_function.py
+++ zfill_function.py
@@ -3,3 +3,12 @@
 # calculate the number of zeros needed
 # add zeros at the beginning of the string
 # print the zero-filled string
+
+def zero_fill(string, width):
+    zeros = max(0, width - len(string))
+    return '0' * zeros + string
+
+user_input_string = input("Enter string: ")
+total_width = int(input("Enter total width: "))
+result = zero_fill(user_input_string, total_width)
+print(f'Result: "{result}"')
