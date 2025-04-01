67e811331eb600fab7ab3f9c6739e8bd96008fd5
unknown <ramosyazmien2@gmail.com>
2025-04-01 11:19:12 +0800

    check if lower

--- islower_function.py
+++ islower_function.py
@@ -2,3 +2,12 @@
 # ask user for input
 # check if all characters are lowercase
 # print result
+def is_lower(string):
+    for char in string:
+        if 'A' <= char <= 'Z':  
+            return False
+    return True  
+
+user_input_string = input("Enter string: ")
+result = is_lower(user_input_string)
+print('Result:' ,result)
