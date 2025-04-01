845ce0a8d4cb49fec775b6812df8ed64dc54ec35
unknown <ramosyazmien2@gmail.com>
2025-04-01 11:37:43 +0800

    pseudocode

---
+++ rindex_function.py
@@ -0,0 +1,4 @@
+# create a program that does the same functionality without using rindex() function.
+# ask user for input
+# loop through the string in reverse to find the last occurrence of the substring
+# print the index or an error message if not found
