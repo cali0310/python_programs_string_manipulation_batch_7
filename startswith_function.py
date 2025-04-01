b5328fd86334ac9f8f152a3690d431bf4f2a5dc5
unknown <ramosyazmien2@gmail.com>
2025-04-01 11:22:03 +0800

    check prefix

--- startswith_function.py
+++ startswith_function.py
@@ -2,3 +2,16 @@
 # ask user for input
 # check if the string starts with the given prefix
 # print result
+
+def starts_with(string, prefix):
+    if len(prefix) > len(string):
+        return False
+    for i in range(len(prefix)):
+        if string[i] != prefix[i]:
+            return False
+    return True
+
+user_input_string = input("Enter string: ")
+prefix_to_check = input("Enter prefix to check: ")
+result = starts_with(user_input_string, prefix_to_check)
+print('Result:', result)
