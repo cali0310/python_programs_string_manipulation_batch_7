4d98543584d20b6a15f308807934a02b226ed778
unknown <ramosyazmien2@gmail.com>
2025-04-01 11:23:22 +0800

    pseudocode

---
+++ rjust_function.py
@@ -0,0 +1,5 @@
+# create a program that does the same functionality without using rjust() function.
+# ask user for input
+# calculate the number of spaces needed
+# add spaces at the beginning of the string
+# print the right-justified string
